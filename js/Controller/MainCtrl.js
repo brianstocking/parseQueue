457cdb0e0110deccaeac864cf8bd708b08ae2181
Brian Stocking <brianm.stocking@gmail.com>
2015-02-26 13:54:48 -0700

    done with get delete and post

--- js/Controller/MainCtrl.js
+++ js/Controller/MainCtrl.js
@@ -1,6 +1,33 @@
 var app = angular.module('parseQ')
 
-app.controller('MainCtrl', function($scope){
-    $scope.test = 'hello'
+app.controller('MainCtrl', function($scope, parseService){
+
+    $scope.postData = function(){
+        parseService.postQuestion($scope.questionText).then(function(){
+                console.log($scope.questionText)
+                $scope.questionText =''
+
+            }
+
+        )
+
+getData()
+    }
+
+   var getData = function(){
+        parseService.getData().then(function(question){
+            $scope.questions = question.data.results
+            console.log(question.data.results)
+        })
+
+    }
+    $scope.removeQuestion = function(question){
+
+        parseService.removeQuestion(question.objectId).then(function(){
+    getData()}
+        )
+    }
+
+getData()
 
 })
--- js/services/service.js
+++ js/services/service.js
@@ -1,5 +1,30 @@
 var app = angular.module('parseQ')
 
-app.service('parseService', function(){
+app.service('parseService', function($http){
 
+    this.postQuestion = function(question) {
+
+        return $http({
+            method: 'POST',
+            url: 'https://api.parse.com/1/classes/questions',
+            data: {
+                theQuestion : question
+            }
+
+        })
+    }
+
+    this.getData = function(data){
+        return $http({
+            method: 'GET',
+            url: 'https://api.parse.com/1/classes/questions/'
+        })
+    }
+
+    this.removeQuestion = function(question){
+        return $http({
+            method: 'DELETE',
+            url: 'https://api.parse.com/1/classes/questions/' + question
+        })
+    }
 })
